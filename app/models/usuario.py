2cc4425542038ee193b857430d49882fc65686f3
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-10-21 13:09:32 +0000

    .

---
+++ app/models/usuario.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import declarative_base
+from config.connention import db
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(150))
+    email = Column(String(150), unique=True)
+    senha = Column(String(150))
+
+    def __init__(self, nome:str, email:str, senha:str):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
